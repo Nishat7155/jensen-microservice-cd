a6c62e994fbba85987f08c8b5d990f521bed9f29
Nishat7155 <57759578+Nishat7155@users.noreply.github.com>
2024-05-11 17:00:48 +0200

    fixing iac issues
    Signed-off-by: Nishat7155 <57759578+Nishat7155@users.noreply.github.com>

--- project-setup-tf/buckets.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "allen-demo" {
-  name                        = "allen-demo-tfstates-${var.environment}"
-  location                    = var.location
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = true
-}
-
