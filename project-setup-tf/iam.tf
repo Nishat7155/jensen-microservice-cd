436e8bca4863ee5bfdf4d577f9d1e3f7915f6ba7
Nishat7155 <57759578+Nishat7155@users.noreply.github.com>
2024-05-16 11:25:07 +0200

    Update iam.tf

--- project-setup-tf/iam.tf
+++ project-setup-tf/iam.tf
@@ -1,16 +1,14 @@
-
-
-
-
-resource "google_service_account" "wif_service_account" {
+resource "google_service_account" "jensen-microservice" {
   project      = var.project_id
-  account_id   = "allen-demo"
-  display_name = "Github Actions CICD Service Account for allen-demo"
-  description  = "Service account used for Github Actions from repository allen-demo"
+  account_id   = "jensen"
+  display_name = "allen-demo Cloud Run identity"
+  description  = "Service account used as identity for allen-demo"
 }
 
-resource "google_project_iam_member" "wif_service_account_deploy" {
+
+
+resource "google_project_iam_member" "jensen-microservice" {
+  role    = google_project_iam_custom_role.identity_role.id
+  member  = "serviceAccount:${google_service_account.allen-demo.email}"
   project = var.project_id
-  role    = google_project_iam_custom_role.deploy_role_allen-demo.id
-  member  = "serviceAccount:${google_service_account.wif_service_account.email}"
 }
