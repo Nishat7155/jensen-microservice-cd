f469ba5b58e392cc49c670f3dfc72b27ba9c878e
Nishat7155 <57759578+Nishat7155@users.noreply.github.com>
2024-05-16 11:30:41 +0200

    Update iam.tf

--- project-setup-tf/iam.tf
+++ project-setup-tf/iam.tf
@@ -1,14 +1,14 @@
 resource "google_service_account" "jensen-microservice" {
   project      = var.project_id
   account_id   = "jensen"
-  display_name = "allen-demo Cloud Run identity"
-  description  = "Service account used as identity for allen-demo"
+  display_name = "Cloud Run identity"
+  description  = "Service account used as identity for pdf-converter"
 }
 
 
 
 resource "google_project_iam_member" "jensen-microservice" {
   role    = google_project_iam_custom_role.identity_role.id
-  member  = "serviceAccount:${google_service_account.allen-demo.email}"
+  member  = "serviceAccount:${google_service_account.jensen-dev.iam.gserviceaccount.email}"
   project = var.project_id
 }
